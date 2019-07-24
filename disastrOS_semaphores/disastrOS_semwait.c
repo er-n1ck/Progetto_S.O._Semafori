745443707a4dc3130db5a0e64268229bcaca3176
Nick <lorenzoginnetti@gmail.com>
2019-07-24 10:43:40 +0200

    Good morning semWait

--- disastrOS_semaphores/disastrOS_semwait.c
+++ disastrOS_semaphores/disastrOS_semwait.c
@@ -7,5 +7,16 @@
 #include "disastrOS_semdescriptor.h"
 
 void internal_semWait(){
-  // do stuff :)
+	//I'm doing stuff :)
+	/*Controlli:
+	 1)Controllo se il semaforo è sul processo
+	 2)Controllo se il processo è sul semaforo
+	 3)Controllo se count è >0
+	 VERO: vuol dire che il processo attuale può essere eseguito subito
+	 	 Devo decrementare il count
+	 FALSO:
+	 	 Decremento
+	 	 Aggiungo alla lista di waiting
+	 	 Metto il running in stato di waiting
+	 */
 }
